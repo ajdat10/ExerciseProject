3b4508da06950a9bc522cceda21059cb55374f75
Andrew Cruz <ajdat10@gmail.com>
2021-11-26 14:57:31 -0500

    connected backend with frontend

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios"
 
 export default class CreateUsers extends Component {
   constructor(props) {
@@ -33,6 +34,9 @@ export default class CreateUsers extends Component {
       
 
     console.log(user);
+
+    axios.post('http://localhost:1010/users/add', user)
+    .then(res => console.log(res.data))
     
     this.setState
       ({
